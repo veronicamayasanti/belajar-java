db2eb30b86da5bac9ed55b4426246b20254bf560
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-07-10 10:34:44 +0700

    feat(test):tipe data array

---
+++ 1.JAVA-DASAR/src/Block.java
@@ -0,0 +1,13 @@
+public class Block {
+    public static void main(String[] args) {
+        System.out.println("hello world 1");
+        System.out.println("hello world 2");
+        System.out.println("hello world 3");
+
+        {
+            System.out.println("hello world 4");
+            System.out.println("hello world 5");
+            System.out.println("hello world 6");
+        }
+    }
+}
---
+++ 1.JAVA-DASAR/src/operasiBoolean.java
@@ -0,0 +1,12 @@
+public class operasiBoolean {
+    public static void main(String[] args) {
+        var absen = 75;
+        var nilaiAkhir = 80;
+
+        var lulusAbsen = absen >= 75;
+        var lulusNilaiAkhir = nilaiAkhir >= 75;
+
+        var lulus = lulusAbsen && lulusNilaiAkhir;
+        System.out.println(lulus);
+    }
+}
