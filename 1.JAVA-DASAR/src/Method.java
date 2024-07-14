22575a7b4cbc28ad624adfc2491c60a7b3be64e6
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-07-14 18:58:48 +0700

    feat(test):tipe data array

---
+++ 1.JAVA-DASAR/src/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+    sayHelloWorld();
+    }
+
+    static  void sayHelloWorld(){
+        System.out.println("Hello world 1");
+        System.out.println("Hello world 2");
+        System.out.println("Hello world 3");
+    }
+}
